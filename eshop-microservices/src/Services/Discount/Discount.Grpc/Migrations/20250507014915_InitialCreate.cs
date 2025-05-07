737f1cf310236e7bbc43a88d9991b65ba56c960d
VN\jason.vu <jason.vu@tpfgroup.cc>
2025-05-07 10:56:30 +0700

    orchestrate Dicount.grpc

---
+++ eshop-microservices/src/Services/Discount/Discount.Grpc/Migrations/20250507014915_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Discount.Grpc.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductName = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Amount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "Id", "Amount", "Description", "ProductName" },
+                values: new object[,]
+                {
+                    { 1, 150, "Tactical Combat Boots Discount", "Tactical Combat Boots" },
+                    { 2, 100, "Night Vision Goggles Discount", "Night Vision Goggles" },
+                    { 3, 200, "Tactical Vest Discount", "Tactical Vest" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
--- eshop-microservices/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ eshop-microservices/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,5 +1,92 @@
-﻿namespace Discount.Grpc.Services;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
-public class DiscountService : DiscountProtoServce
+namespace Discount.Grpc.Services;
+
+public class DiscountService(
+    DiscountContext dbContext, 
+    ILogger<DiscountService> logger) 
+    : DiscountProtoService.DiscountProtoServiceBase
 {
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext
+                            .Coupons
+                            .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+        
+        if (coupon is null)
+        {
+            coupon = new Coupon
+            {
+                ProductName = "No Discount",
+                Amount = 0,
+                Description = "No Discount Desc"
+            };
+        }
+        logger.LogInformation("Discount is retrieved for the product: {ProductName}, Amount: {Amount}", coupon.ProductName, coupon.Amount);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = request.Coupon.Adapt<Coupon>();
+        if (coupon is null)
+        {
+            var errDetails = "Invalid request object";
+            logger.LogWarning("Request failed. StatusCode: {StatusCode}, Details: {Details}", StatusCode.InvalidArgument, errDetails);
+            throw new RpcException(new Status(StatusCode.InvalidArgument, errDetails));
+        }
+        dbContext.Coupons.Add(coupon);
+
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Coupon is successfully created. ProductName: {ProductName}, Amount: {Amount}", coupon.ProductName, coupon.Amount);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = request.Coupon.Adapt<Coupon>();
+        if (coupon is null)
+        {
+            var errDetails = "Invalid request object";
+            logger.LogWarning("Request failed. StatusCode: {StatusCode}, Details: {Details}", StatusCode.InvalidArgument, errDetails);
+            throw new RpcException(new Status(StatusCode.InvalidArgument, errDetails));
+        }
+        dbContext.Coupons.Update(coupon);
+
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Coupon is successfully updated. ProductName: {ProductName}, Amount: {Amount}", coupon.ProductName, coupon.Amount);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = dbContext.Coupons.FirstOrDefault(x => x.ProductName == request.ProductName);
+        if (coupon is null)
+        {
+            var errDetails = "Coupon not found";
+            logger.LogWarning("Request failed. StatusCode: {StatusCode}, Details: {Details}", StatusCode.NotFound, errDetails);
+            throw new RpcException(new Status(StatusCode.NotFound, errDetails));
+        }
+
+        dbContext.Coupons.Remove(coupon);
+
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Coupon is successfully deleted. ProductName: {ProductName}, Amount: {Amount}", coupon.ProductName, coupon.Amount);
+
+        return new DeleteDiscountResponse { Success = true };
+    }
 }
