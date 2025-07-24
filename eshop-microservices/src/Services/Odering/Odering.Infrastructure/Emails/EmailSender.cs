f4795f9baf76eb06b7c317a6850095f4f528c9d0
VN\jason.vu <jason.vu@tpfgroup.cc>
2025-07-24 16:23:43 +0700

    Add notification Event

--- eshop-microservices/src/Services/Odering/Odering.Infrastructure/Emails/EmailSender.cs
+++ eshop-microservices/src/Services/Odering/Odering.Infrastructure/Emails/EmailSender.cs
@@ -1,12 +1,35 @@
-﻿using Ordering.Application.Configuration.Emails;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Ordering.Application.Configuration.Emails;
 
 namespace Odering.Infrastructure.Emails;
 
 public class EmailSender : IEmailSender
 {
-    public async Task SendEmailAsync(EmailMessage message)
+    private readonly EmailsSettings _emailsSettings;
+    public EmailSender(EmailsSettings emailsSettings)
     {
-        // Integration with email service.
+        _emailsSettings = emailsSettings;
+    }
+    public async Task SendEmailAsync(EmailMessage emailMessage)
+    {
+        // Integration with email service
+        // use Mailkit
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress(_emailsSettings.SenderName, emailMessage.From ?? _emailsSettings.SenderEmail));
+        message.To.Add(MailboxAddress.Parse(emailMessage.To));
+        message.Subject = emailMessage.Subject;
+
+        message.Body = new TextPart("html")
+        {
+            Text = emailMessage.Content
+        };
+
+        using var client = new SmtpClient();
+        await client.ConnectAsync(_emailsSettings.SmtpServer, _emailsSettings.SmtpPort, MailKit.Security.SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(_emailsSettings.SenderEmail, _emailsSettings.Password);
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
 
         return;
     }
--- eshop-microservices/src/Services/Odering/Ordering.Application/Configuration/Emails/EmailsSettings.cs
+++ eshop-microservices/src/Services/Odering/Ordering.Application/Configuration/Emails/EmailsSettings.cs
@@ -1,6 +1,15 @@
-﻿namespace Ordering.Application.Configuration.Emails;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ordering.Application.Configuration.Emails;
 
 public class EmailsSettings
 {
-    public string FromAddressEmail { get; set; } = string.Empty;
+    //public string FromAddressEmail { get; set; } = string.Empty;
+    [Required]
+    public string SmtpServer { get; set; } = string.Empty;
+    [Required]
+    public int SmtpPort { get; set; }
+    public string SenderName { get; set; } = string.Empty;
+    public string SenderEmail { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
